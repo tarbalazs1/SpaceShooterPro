4e02379f2ff8a9f7909979d0c2eb6200d3562c5f
Balazs Tar <tarbalazs1@gmail.com>
2024-12-28 16:48:47 +0000

    Explode the asteroid

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -6,6 +6,8 @@ public class Asteroid : MonoBehaviour
 {
     [SerializeField]
     private float _rotationSpeed = 20.0f;
+    [SerializeField]
+    private GameObject _explosionPrefab;
 
     // Start is called before the first frame update
 
@@ -17,7 +19,16 @@ public class Asteroid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, _rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.forward * _rotationSpeed * Time.deltaTime);
+    }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Laser")
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(this.gameObject, 0.25f);
+        }
     }
 }
